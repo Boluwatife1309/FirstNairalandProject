d4edaf941cf39f00243d6fb4e4d2341a97ba9c22
Boluwatife1309 <Tiwadayooladunni2000@gmail.com>
2023-01-31 12:14:18 +0100

    done

---
+++ script.js
@@ -0,0 +1,60 @@
+/*
+var a = 5;
+try {
+   console.log( b + 1) 
+} catch (error) {
+    console.log(error)
+}
+console.log("hello")
+
+function calc(){
+    try {
+    } catch (l) {  
+    }
+}
+ */
+/*
+function handleclick (){
+    alert("hello world")
+}
+
+function handleinput( a, b){
+    alert( a + b)
+
+}
+
+const handleclick = () => 2 + 3 
+console.log(handleclick())
+
+function car(){
+    return("hello")
+}
+console.log(car())
+*/
+
+
+document.getElementById("btn").onclick =function (){
+    alert("hello")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
